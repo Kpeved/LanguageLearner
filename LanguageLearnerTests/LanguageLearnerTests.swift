74a19aa7d19cc4a2577da5592953c369f7ef1d71
kpeved <uakpevedko@gmail.com>
2026-05-28 16:14:56 +0100

    Initial Commit

---
+++ LanguageLearnerTests/LanguageLearnerTests.swift
@@ -0,0 +1,19 @@
+//
+//  LanguageLearnerTests.swift
+//  LanguageLearnerTests
+//
+//  Created by Michail Kulaga on 28.05.2026.
+//
+
+import Testing
+@testable import LanguageLearner
+
+struct LanguageLearnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
